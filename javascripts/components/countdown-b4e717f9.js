51ecdec95389df12f8633778516198f025144a9b
Jun Lin <linjunpop@gmail.com>
2014-02-08 17:20:28 +0800

    Automated commit at 2014-02-08 09:20:28 UTC by middleman-deploy 0.1.3

--- javascripts/components/countdown-b4e717f9.js
+++
@@ -1,1 +0,0 @@
-!function(){var e,n,t,a,o,r,c;(e=document.querySelector(".countdown"))&&(c=new Date,o=new Date("January 1, 2014"),a=864e5,r=o.getTime()-c.getTime(),t=r/a,n=Math.floor(t),e.innerHTML=n)}.call(this);
---
+++ javascripts/components/countdown-c0a11558.js
@@ -0,0 +1,1 @@
+!function(){var e,n,t,a,o,r;(e=document.querySelector(".countdown"))&&(r=new Date,a=new Date("January 1, 2014"),t=864e5,o=r.getTime()-a.getTime(),n=Math.floor(o/t),e.innerHTML=n)}.call(this);
